c5672a66220e711a146335f5e6f0f45bebc5076a
Aftab Ahmed <aftabh150@gmail.com>
2025-09-06 17:16:16 +0100

    Added User list and Delete Function

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,20 +3,22 @@ import UserCrudApp from "./UserCrud";
 
 const Form = () => {
   const [user, setUser] = useState({ name: "", age: "" });
-  console.log(user.name);
+  const [listOfUsers, setListOfUsers] = useState([]);
 
   function handleChange(e) {
-    console.log("Event > ", e.target);
+    // console.log("Name of Field > ", e.target.name); ====== can check the name of field ======
     const { name, value } = e.target;
-    setUser((prev) => ({
-      ...prev, // keep other fields unchanged
-      [name]: value, // update only the name
-    }));
+    setUser({ ...user, [name]: value });
   }
 
   function handleSubmit(e) {
     e.preventDefault();
-    alert(user.name);
+    setListOfUsers([...listOfUsers, user]);
+    setUser({ name: "", age: "" });
+  }
+
+  function deleteUser(id) {
+    setListOfUsers(listOfUsers.filter((user, i) => i !== id));
   }
 
   return (
@@ -61,6 +63,32 @@ const Form = () => {
         <div className="border-t border-gray-300 my-4" />
 
         {/* <UserCrudApp /> */}
+
+        <div className="flex flex-col items-center">
+          <h3>Here are the users</h3>
+          {listOfUsers.map((user, index) => (
+            <div
+              key={index}
+              className="border-2 border-gray-200 p-4 m-3 list-none w-50"
+            >
+              <li>{user.name}</li>
+              <li>ID - {index}</li>
+              <li>Age - {user.age}</li>
+              <button
+                className="border-2 border-gray-200 p-1 mt-2 cursor-pointer rounded-md bg-red-500 text-white w-20"
+                onClick={() => deleteUser(index)}
+              >
+                Delete
+              </button>
+              <button
+                className="border-2 border-gray-200 ml-2 p-1 cursor-pointer rounded-md bg-blue-300 w-15"
+                onClick={() => console.log("Clicked on Edit Button")}
+              >
+                Edit
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
